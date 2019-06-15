fd8f6669d6d28b7537d05f7c8d15a367dbc78758
taaaka <Rw.taaaka@gmail.com>
2019-06-15 11:28:48 +0900

    [chore] もろもろ追加

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    // prettier
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    // eslint
+    eqeqeq: 'error',
+    'no-console': 'error',
+
+    // eslint-typescript
+    '@typescript-eslint/no-magic-numbers': 'warn',
+  },
+};
---
+++ src/sample.ts
@@ -0,0 +1,27 @@
+function greet(message: string): string {
+  if (!message) {
+    return 'You are anonymous!';
+  } else {
+    return `Hello, ${message}!!`;
+  }
+}
+
+interface Test {
+  property: boolean;
+  count: number;
+  name: string;
+}
+
+let count = 10;
+const str = 'hoge';
+
+if (10 === count) {
+  count = 2;
+  // console.log(str);
+}
+
+const name = 'abc';
+
+// console.log(greet(name));
+
+export default greet;
